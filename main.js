e00db1e673eac1f6a74a8a00057d79bea4674f37
Okba CEMUZ <birboshok@gmail.com>
2022-01-20 19:27:20 +0300

    new commiit

--- main.js
+++ main.js
@@ -1,31 +1,6 @@
+const editSocialButtons = () => {
+    let btns = document.getElementsByClassName("accountButton");
+    console.log(btns);
+}
 
-// const hello = () => {
-//     console.log("iam here");
-// }
-
-// const editSocialButtons = () => {
-//     // document.getElementsByClassName("accountButton").forEach(element => {
-//     //     console.log(element);
-//     // });
-//     let btns = document.querySelectorAll("accountButton");
-//     console.log(btns);
-//     Array.from(btns).forEach(function (element) {
-//         console.log(element)
-//     });
-
-//     // [].forEach.call(btns, (btn) => {
-//     //     console.log(btn)
-//     // })
-
-// }
-
-
-// window.addEventListener("load", editSocialButtons);
-
-$(document).ready(function () {
-
-    $(".accountButton").on("click",function(e){
-        e.preventDefault();
-        alert("test")
-    })
-})
+editSocialButtons();
