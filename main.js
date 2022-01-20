65ee325a8b810477c65b13ace5dd0ba0da329b45
Okba CEMUZ <birboshok@gmail.com>
2022-01-20 19:58:52 +0300

    ewdew

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const editSocialButtons = () => {
-    let btns = document.getElementsByClassName("accountButton");
-    console.log(btns);
+    var btns = document.querySelectorAll('button[class=accountButton]');
+    for (var i = 0; i < btns.length; i++) {
+        console.log(btns[i]);
+    }
 }
 
 editSocialButtons();
